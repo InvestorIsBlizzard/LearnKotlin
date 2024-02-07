a621bf9897d207f0c6ec2b6a38ace8d736894ff0
smertobro <us62ru41@mail.ru>
2024-02-07 12:02:43 +0300

    Внес изменения

--- src/main/kotlin/lesson1.kt
+++ src/main/kotlin/lesson1.kt
@@ -8,4 +8,24 @@ fun main(args: Array<String>) {
 
    println(numberOfOrders)
     println(notificationText)
+
+    // числовые (только целые числа)
+    val intNum1: Int = -2147483648 // 32 bit - max число в 32 битах
+    val intNum2: Int = -2147483647 // 32 bit 47 потому что в положительные числа входит "0"
+    val longNum: Long // 64 bit
+    val shortNum: Short // 16 bit
+    val byteNum: Byte // 8 bit
+    val unsignedNum: UInt = 2147483648u // 32 bit
+
+    // вещественные
+    val doubleNum: Double = 234.42 // 64 bit
+    val floatNum: Float = 84.2f // 32 bit
+
+    // строковые
+    val stringValue: String = "Здесь могла быть ваша реклама"
+    val charValue: Char = '1'
+
+    // логические
+    val trueValue: Boolean = true
+    val falseValue: Boolean = false
 }
--- src/main/kotlin/lesson2.kt
+++ src/main/kotlin/lesson2.kt
@@ -1,3 +1,66 @@
 fun main() {
-    println("Я слежу за вами")
+    val a = 5
+    val b = 7
+    val sum = a + b
+
+ //  val c - не получится
+
+    val c1 = 888
+    val c2: Int
+
+// println(a + b)
+// println (sum)
+// println (10 / 3)
+
+    val intNum1: Int = 10 // - целые числа
+    val intNum2: Int = 3
+//    println (intNum1 / intNum2)
+
+    val floatNum1: Float = 10f // дробные, с меньшим колл-вом чисел после ,
+    val floatNum2: Float = 3f
+//    println (floatNum1 / floatNum2)
+
+    val doubleNum1: Double = 10.0 // тоже дробные , но уже с большим колличеством
+    val doubleNum2: Double = 3.0
+//    println (doubleNum1 / doubleNum2)
+
+    // остаток от деления %
+
+//    println(10 % 3)
+//    println(10 % 2)
+
+    // конечный тип переменной
+
+    val c = intNum1 + floatNum1
+//    println(c::class.simpleName) // float - 4 bit
+
+    val d = intNum1 + floatNum1 + doubleNum1
+//    println(d::class.simpleName) // double - 8 bit
+
+   // инкремент, декремент
+
+    var counter = 0
+
+    counter = counter + 1
+    counter += 1
+    counter ++
+
+    println (counter)
+
+    counter -= 1
+    counter--
+
+    ++counter
+    --counter
+
+    // операторы сравнения
+    // a = 5, b = 7
+
+    println(a > b)
+    println(a < b)
+    println(a >= b)
+    println(a <= b)
+    println(a == b)
+    println(a != b) // != - знак не равно
+
 }
