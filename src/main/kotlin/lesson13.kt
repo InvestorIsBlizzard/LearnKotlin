bc282b91d6fb44fc25db9bb9a5daa8bc0e6d415a
smertobro <us62ru41@mail.ru>
2024-02-12 02:05:01 +0300

    Урок 13: Null Safety – операторы ?. !!, NullPointerException (NPE)

---
+++ src/main/kotlin/lesson13.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    // NullPointerException - исключение нулевого показателя ИНП (NPE)
+    // null
+
+    val nullableString: String? = null
+    val nonNullableString: String = "some string"
+
+//    println(nullableString?.length) // .? - оператор безопасного вызова, т.к может быть значение null и чтобы все не крашнуло он оберегает
+//    println(nonNullableString?.length) // / .length  заменяет if else
+
+ // if(nullableString != null) println(nullableString.length)
+ // esle println("Переменная хранит null")
+
+//    val nullableLine: String? = readLine()
+
+//    val stringLength: Int = nullableString?.length ?: 999  // ?: - оператор элвис, заменяет esle, возвращает значение определенного типа
+//    println(stringLength)
+
+    val stringLength: Int = nullableString?!!.length // !! оператор утверждения, который говорит о том, что null точно быть не может и что все будет ок))
+
+
+
+}
