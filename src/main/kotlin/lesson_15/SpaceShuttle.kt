a6cf0e1f8a3b8b6e9eef2703ab44e08794736fd8
smertobro <us62ru41@mail.ru>
2024-02-12 04:28:01 +0300

    Урок 15: Интерфейсы, абстрактные классы. Имплементация

---
+++ src/main/kotlin/lesson_15/SpaceShuttle.kt
@@ -0,0 +1,30 @@
+package lesson_15
+
+abstract class SpaceShuttle : Movable, Shootable{
+
+    abstract val tankSize: Int
+    abstract fun runDiagnostic()
+
+}
+
+class Upsilon(override val tankSize: Int) : SpaceShuttle() { // upsilon - класс наследник, который наследует базовый класс SpaseShuttle
+    override fun runDiagnostic() {
+        println("Диагностика запущена")
+    }
+
+    override fun prepareForTakeOff() {
+        println("Развернуть крылья")
+    }
+
+    override fun prepareForLanding() {
+        println("Свернуть крылья")
+    }
+
+    override fun startShooting() {
+        println("Начать стрельбу")
+    }
+
+    override fun reloadGuns() {
+        println("Перезарядить орудия")
+    }
+}
---
+++ src/main/kotlin/lesson_15/lesson15.kt
@@ -0,0 +1,13 @@
+package lesson_15
+
+fun main() {
+
+    val upsilonShuttle = Upsilon( 50)
+
+    upsilonShuttle.runDiagnostic()
+    upsilonShuttle.prepareForTakeOff()
+    upsilonShuttle.prepareForLanding()
+    upsilonShuttle.startShooting()
+    upsilonShuttle.reloadGuns()
+    
+}
